572ca8586f9543e822efd663b06e0c716823d64c
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-11 16:59:07 -0400

    FEAT : added searchFeed.jsx , edit App.js for searchFeed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Stack, Box } from '@mui/material'
 import { VideoCard, ChannelCard } from './'
 const Videos = ({ videos }) => {
-
-
 	return (
 		<Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
 			{videos.map((item, idx) => (
